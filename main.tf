d8aff07bdfa1a22ff9c715f11142d6d32da51994
__eMCe__ <93644245+MarieeCzy@users.noreply.github.com>
2023-02-23 14:18:51 +0100

    Add variables.tf

--- main.tf
+++ main.tf
@@ -8,12 +8,28 @@ terraform {
 }
 
 provider "google" {
-  project = "focus-poet-376519"
+  project = var.project
 }
 
 resource "google_service_account" "service_account" {
-    account_id = "dbt-service-account"
-    display_name = "dbt-service-account"
-    description = "Service account for dbt cloud"
+  account_id   = var.service_account_id
+  display_name = var.service_account_display_name
+  description  = "Service account for dbt cloud"
+}
+
+resource "google_project_iam_member" "my_service_account_roles" {
+  project = var.project
+  role    = "roles/bigquery.admin"
+  member  = "serviceAccount:${google_service_account.service_account.email}"
+}
+
+resource "google_service_account_key" "my_service_account_key" {
+  service_account_id = google_service_account.service_account.name
+  public_key_type    = "TYPE_X509_PEM_FILE"
+}
+
+output "my_service_account_key_json" {
+  value     = google_service_account_key.my_service_account_key.private_key
+  sensitive = true
 }
 
