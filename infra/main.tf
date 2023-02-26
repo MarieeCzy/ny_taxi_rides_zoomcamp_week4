5c7a2320b0876625f75a28856483e1e029f620da
__eMCe__ <93644245+MarieeCzy@users.noreply.github.com>
2023-02-26 19:55:03 +0100

    Modify GCP- BigQuery, Service Account, Cloud Storage infrastructure

--- infra/main.tf
+++ infra/main.tf
@@ -7,6 +7,7 @@ terraform {
   }
 }
 
+# IAM Service Account - BigQuery Admin
 provider "google" {
   project = var.project
 }
@@ -33,3 +34,36 @@ output "my_service_account_key_json" {
   sensitive = true
 }
 
+#Cloud Storage Buckets
+resource "google_storage_bucket" "de-zoomcamp-bucket" {
+  name     = var.bucket_name
+  location = var.location
+}
+
+#BigQuery datasets
+resource "google_bigquery_dataset" "dbt-dataset-maria" {
+  dataset_id = "dbt_maria"
+  location   = var.location
+}
+
+resource "google_bigquery_dataset" "dbt-dataset-production" {
+  dataset_id = "production"
+  location   = var.location
+}
+
+resource "google_bigquery_dataset" "dbt-dataset-staging" {
+  dataset_id = "staging"
+  location   = var.location
+}
+
+resource "google_bigquery_dataset" "dbt-dataset-trips-data" {
+  dataset_id = "trips_data_all"
+  location   = var.location
+}
+
+#BigQuery tables
+resource "google_bigquery_table" "default" {
+  dataset_id = google_bigquery_dataset.dbt-dataset-trips-data.dataset_id
+  count      = length(var.big-query-tables)
+  table_id   = var.big-query-tables[count.index]
+}
--- infra/variables.tf
+++ infra/variables.tf
@@ -9,3 +9,16 @@ variable "service_account_id" {
 variable "service_account_display_name" {
   default = "dbt-service-account"
 }
+
+variable "bucket_name" {
+  default = "dbt-bucket-de-zoomcamp"
+}
+
+variable "big-query-tables" {
+  type    = list(any)
+  default = ["green_trip_data_2021_01", "yellow_trip_data_2021_01"]
+}
+
+variable "location" {
+  default = "US"
+}
