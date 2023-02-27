814d74b9b57f906f137a2efbc2f4f1b90d8ff73c
__eMCe__ <93644245+MarieeCzy@users.noreply.github.com>
2023-02-27 23:02:32 +0100

    Add etl functions

--- etl/main_etl_script.py
+++ etl/main_etl_script.py
@@ -2,10 +2,83 @@ from pathlib import Path
 import pandas as pd
 from prefect import flow, task
 from prefect_gcp.cloud_storage import GcsBucket
+import os
 
-@flow 
+@task
+def fetch_data(dataset_url: str) -> pd.DataFrame:
+    '''
+    Read taxi data from web into pandas DataFrame.
+    
+    Args:
+        dataset_url (str): Url source of data
+        
+    Notes:
+        Backup taxi data files in .csv format: 
+        https://github.com/DataTalksClub/nyc-tlc-data
+    '''
+    df = pd.read_csv(dataset_url)
+    return df
+
+@task
+def pre_clean_df(df: pd.DataFrame) -> pd.DataFrame:
+    '''
+    Fix some dtype issues.
+    
+    Args: 
+        df (pd.Dataframe): Raw taxi dataframe downloaded from web.
+    '''
+    df['tpep_pickup_datetime'] = pd.to_datetime(df['tpep_pickup_datetime'])
+    df['tpep_dropoff_datetime'] = pd.to_datetime(df['tpep_dropoff_datetime'])
+    return df
+
+
+@task
+def write_local(df: pd.DataFrame, color: str, dataset_file: str) -> Path:
+    '''
+    Write pre cleaned Data Frame locally as .parquet file.
+    
+    Args:
+        df (pd. DataFrame): Pre cleaned DataFrame
+        color (str): The collection from which the data comes
+        dataset_file (str): Name of .parquet file
+    '''
+    
+    path = Path(f'data/{dataset_file}.paquet')
+    df.to_parquet(path, compression='gzip')
+    return path
+
+@task
+def write_to_gcs(path: Path) -> None:
+    '''
+    Uploading local parquet file to Google Cloud Storage bucket.
+    
+    Args:
+        path (Path): URL path to .parquet taci dataset daved locally in /data directory.
+    '''
+    gcp_cloud_storage_bucket_block = GcsBucket.load("dbt-gcs-bucket")
+    gcp_cloud_storage_bucket_block.upload_from_path(from_path=path, to_path=path)
+
+@flow
 def etl_web_to_gcs(year: int, month: int, color: str) -> None:
-    print(year, month, color)
+    '''
+    The main sub-flow function managing the tasks of:
+    - downloading datasets, 
+    - pre-cleaning,
+    - local saving,
+    - loading to Google Cloud Storage.
+    
+    Args:
+        year (int): The year the data comes from
+        month (int): The month the data comes from
+        color (str): The collection from which the data comes
+    '''
+    dataset_file = f'{color}_tripdata_{year}-{month:02}'
+    dataset_url = f'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/{color}/{dataset_file}.csv.gz'
+    
+    df = fetch_data(dataset_url)
+    clean_df = pre_clean_df(df)
+    local_path = write_local(clean_df, color, dataset_file)
+    write_to_gcs(local_path)
 
 @flow (log_prints=True, retries=3)
 def etl_main_flow(months: list = [1,2,3,4],
@@ -19,10 +92,11 @@ def etl_main_flow(months: list = [1,2,3,4],
     Args:
         months (list): The months the data comes from
         year (int): The year the data comes from
-        color (list): The collection from which the data comes
+        colors (list): The collections from which the data comes
     
     Notes:
         See: https://www.nyc.gov/site/tlc/about/tlc-trip-record-data.page for more info.
+        Data from official page available in .parquet format.
     '''
     for color in colors:
         for month in months:
